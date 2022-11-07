77f4dfb2ccd96486ad73a936099091d3c5346f53
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2022-11-07 14:45:49 +0200

    My solution (#1)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+import styles from './App.module.css';
+
 function App() {
   return (
     <>
+      <div className={styles.decoration}></div>
       <CharacterEditor />
       <Footer />
     </>
